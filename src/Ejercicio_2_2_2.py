b995ceea8b0d61fc338fb525dce66ae43d0b638f
avilrod3004 <avilrod3004@g.educaand.es>
2023-10-31 14:26:48 +0100

    Terminado y con test

--- src/Ejercicio_2_2_2.py
+++ src/Ejercicio_2_2_2.py
@@ -2,28 +2,7 @@
 Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
 """
 
-# from Ejercicio_2_1_1 import pedirEdad (da error al hacer el test de aniosCumplidos)
-def pedirEdad():
-    """
-    Pedir la edad y comprobar que se trata de un número entero entre 1 y 100
-    
-    Retorna
-    -------
-    int
-        un entero con el valor de la edad introducida por consola
-    """
-    salir = False
-    while not salir:
-        entrada = input("Introduzca su edad: ")
-
-        if entrada.isnumeric() and 0 < int(entrada) <= 100:
-            salir = True
-        else:
-            print("**ERROR** - LA EDAD DEBE SER ENTRE O Y 100")
-    
-    edad = int(entrada)
-
-    return edad
+from src.Ejercicio_2_1_1 import pedirEdad
 
 
 def aniosCumplidos(edad: int):
