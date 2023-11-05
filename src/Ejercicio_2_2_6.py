3e551a420b69dab48acb28e397e9315798a532d9
avilrod3004 <avilrod3004@g.educaand.es>
2023-11-05 18:04:19 +0100

    Terminado (no tiene test)

--- src/Ejercicio_2_2_6.py
+++ src/Ejercicio_2_2_6.py
@@ -1,5 +1,5 @@
 '''
-Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo, de altura el número introducido. 
+Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo, de altura el número introducido.  
 
 *
 **
