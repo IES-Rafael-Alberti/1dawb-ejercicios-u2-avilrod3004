e233d75c3c7ce3bd2593c569f1d029368c77d487
avilrod3004 <avilrod3004@g.educaand.es>
2023-11-05 18:03:02 +0100

    Terminado (no tiene test)

--- src/Ejercicio_2_2_8.py
+++ src/Ejercicio_2_2_8.py
@@ -1,5 +1,11 @@
 '''
 Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo. 
+
+1
+3 1
+5 3 1
+7 5 3 1
+9 7 5 3 1
 '''
 
 def pedirNum():
@@ -13,8 +19,15 @@ def pedirNum():
 
     return int(num)
 
+
 def main():
     num = pedirNum()
+    serie = ''
+
+    for i in range(1, (num + 1), 2):
+        serie = f'{i} ' + serie
+        print(serie)
+        i += 2
 
 
 if __name__ == '__main__':
